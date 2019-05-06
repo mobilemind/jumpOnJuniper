09730858c8ba199b833bebe91000f9dc46bd892c
Tom King <mobilemind@users.noreply.github.com>
2019-05-06 13:07:37 -0700

    switch to convenience build of uglify; delete lockfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,7 +188,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-csslint");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-uglify-es");
+    grunt.loadNpmTasks("@sailshq/grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-rename");
     grunt.loadNpmTasks("grunt-eslint");
     grunt.loadNpmTasks("grunt-html-minify");
